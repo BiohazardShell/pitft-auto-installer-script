741ba9bd0b05dc51f47df4b261e4511d9d222711
Alexis AGGERY <alexis.aggery@gmail.com>
2018-03-18 16:00:00 +0100

    update installer script

--- pitft-installer.sh
+++ pitft-installer.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-wget https://www.dropbox.com/s/npjc52s2z6ifk55/pitft35r-overlay.dtb?dl=0 -o pitft35r-overlay.dtb
-
 sudo mv pitft35r-overlay.dtb /boot/overlays/
-
-rm pitft35r-overlay.dtb?dl=0
 
 wget https://raw.githubusercontent.com/adafruit/Adafruit-PiTFT-Helper/master/adafruit-pitft-helper2.sh
 
